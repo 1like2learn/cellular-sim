b8138c004b964bee1d9b4c7391f2600eb825bb7f
1like2learn <ndespained@gmail.com>
2020-09-21 11:51:46 -0700

    functionButtons base function is done

--- src/component/functionButtons.js
+++ src/component/functionButtons.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { useform, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 export default function FunctionButtons({setCoord}){
-  const { handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    
+    setCoord({
+      row: data.row,
+      column: data.column
+    })
+  }
   return (
     <div>
       <form onSubmit ={handleSubmit(onSubmit)}>
-
+        <label>Define Grid Dimensions</label>
+        <input
+          name = "row"
+          ref = {register({
+            required: true,
+            pattern: /^[1-9]\d*\.?[0]*$/
+          })}
+          type = "number"
+        />
+        {errors.row && <span>Please enter a positive whole number.</span>}
+        <input 
+          name = "column" 
+          ref = {register({
+            required: true,
+            pattern: /^[1-9]\d*\.?[0]*$/
+          })}
+          type = "number"
+        />
+        {errors.column && <span>Please enter a positive whole number.</span>}
+        <button type = "submit">Define Grid Size</button>
       </form>
-
     </div>
   )
 }
