be9bb36d000d7b77cc2307a8cc75aabcb80057ed
1like2learn <ndespained@gmail.com>
2020-09-21 15:36:09 -0700

    addCell made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
-import SimulationField from './component/simulationField'
-import FunctionButtons from './component/functionButtons'
+import SimulationField from './component/simulationField';
+import FunctionButtons from './component/functionButtons';
 import './App.css';
 const defaultCoord = {row: 0, column: 0 }
 function App() {
--- src/component/simulationField.js
+++ src/component/simulationField.js
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cell from '../helpers/cells'
 
 export default function SimulationField({row, column}){
+  const [ grid, setGrid ] = useState({})
+  let c = 0;
+  let r = 0;
+  let tempGrid = {}
+  while (r < row){
+    if (r === 0) {
+      const tempCell = Cell(r, c)
+      tempCell.tL = false
+      tempCell.t = false
+      tempCell.tR = false
+      tempCell.l = false
+      tempCell.Bl = false
+      tempGrid[`${r}${c}`] = tempCell
+    }
+    else if(r === row - 1){
+      const tempCell = Cell(r, c)
+      tempCell.tL = false
+      tempCell.t = false
+      tempCell.tR = false
+      tempCell.r = false
+      tempCell.bR = false
+      tempCell.l = tempGrid[`${r-1}${c}`]
+      tempGrid[`${r}${c}`] = tempCell
+    }
+    else{
+      const tempCell = Cell(r, c)
+      tempCell.tL = false
+      tempCell.t = false
+      tempCell.tR = false
+      tempCell.bR = false
+      tempCell.l = tempGrid[`${r-1}${c}`]
+      tempGrid[`${r}${c}`] = tempCell
+      tempGrid[`${r-1}${c}`].r = tempCell
+    }
+  };
 
   return(
     <div>
